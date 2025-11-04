dcc62aaae2efef07738fb02d1ac408a378d22e11
Валерия Пономарева <ponomarevavaleria86@gmail.com>
2025-11-04 21:51:13 +0300

    Add files via upload

---
+++ hw8.c
@@ -0,0 +1,15 @@
+﻿#define _CRT_SECURE_NO_DEPRECATE
+#include <stdio.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "RUS");
+	int N, i;
+	double sum = 0.0;
+	printf("Введите число N: ");
+	scanf("%d", &N);
+	for (i = N; i <= 2 * N; i++) {
+		sum += (double)(i * i);
+	}
+	printf("Сумма выражения равна %.6lf", sum);
+}
